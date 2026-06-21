7d004c94d1e3f46b8bc1117f96c1af3d231aa5d1
vasvince98 <vas.vince98@gmail.com>
2026-06-21 16:49:32 +0200

    drive service added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,17 @@ module com.vasvince.project_loader {
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
     requires org.slf4j;
+    requires com.google.api.client;
+    requires com.google.api.client.json.gson;
+    requires com.google.api.services.drive;
+    requires com.google.api.client.auth;
+    requires google.api.client;
+    requires com.google.api.client.extensions.jetty.auth;
+    requires com.google.api.client.extensions.java6.auth;
+    requires jdk.httpserver;
 
     opens com.vasvince.project_loader to javafx.fxml;
     exports com.vasvince.project_loader;
+    exports com.vasvince.project_loader.services;
+    opens com.vasvince.project_loader.services to javafx.fxml;
 }
